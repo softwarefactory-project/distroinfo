c3c7872fa8f81f9713a20e622fb8d84090c4ba8a
Jakub Ružička <jruzicka@redhat.com>
2018-06-27 18:30:37 +0200

    core: enable remote import of info files
    New `remote-info` section allows a definition of remote info repos. It
    is a dictionary with each entry being a dict of arguments needed to
    construct dedicated DistroInfo instance to fetch the remote files. The
    key of each entry can then be used in `import` section to import info
    files from the remote info repositories.
    
    For example you can define rdoinfo in `remote-info`:
    
        remote-info:
          rdoinfo:
            remote_info: https://raw.githubusercontent.com/redhat-openstack/rdoinfo/master/
    
    and import any file from it using following syntax:
    
        import:
        - rdoinfo -> rdo-full.yml
    
    Change-Id: I74e6cb3007fd8a30c6f5a8ea7089441a86225993

---
+++ tests/unit/test_remote_info.py
@@ -0,0 +1,15 @@
+from distroinfo.info import DistroInfo
+from distroinfo.query import get_package
+
+import tests.test_common as common
+
+
+def test_remote_info():
+    di = DistroInfo('remote.yml',
+                    local_info=common.get_test_info_path('minimal'))
+    info = di.get_info()
+    edf = info['package-default'].get('extra-default-field')
+    assert edf == 'foo-default-%(project)s'
+    nova = get_package(info, 'openstack-nova')
+    assert nova['extra-default-field'] == 'foo-default-nova'
+    assert nova['extra-conf-field'] == 'foo-conf-nova'
